e26d195b80b9f0173329d471ca2a080c1e1020c0
lynasberaich <lynab1@umbc.edu>
2025-02-05 12:46:39 -0500

    Create main.h

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "memory.h"
+#include "cg.h"
+
+#ifdef PARALLEL
+#include <mpi.h>
+#endif
+
+#ifndef M_PI
+#define M_PI          (3.1415926535897932384626433832795029)
+#endif
+
+#endif
+
+
